41ee01bbf37a3c9560bd3bdc1acf81aa008e8b04
minju0236 <minju0236@gmail.com>
2025-09-07 23:51:16 +0900

    feat: 메뉴바 로그인 전후 상태관리 추가

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -39,7 +39,7 @@ const Menu = () => {
   useEffect(() => {
     const path = location.pathname;
 
-    if (path === '/todayWeather') setActiveMenu('WEATHER');
+    if (path === '/today-weather') setActiveMenu('WEATHER');
     else if (path === '/looktoday') setActiveMenu('LOOKTODAY');
     else if (path === '/lookbook') setActiveMenu('LOOKBOOK');
     else if (['/login', '/sign-up', '/sign-up-complete'].includes(path))
@@ -117,59 +117,6 @@ const Menu = () => {
               className={activeMenu === 'LOGIN' ? 'active-menu' : ''}
             >
               LOGIN
-    const [activeMenu, setActiveMenu] = useState('');
-    const [isHovered, setIsHovered] = useState(false);
-    const navigate = useNavigate();
-    const location = useLocation();
-
-    // 메뉴 선택 시 active 반영을 위함
-    useEffect(() => {
-        const path = location.pathname;
-
-        if (path === '/today-weather') setActiveMenu('WEATHER');
-        else if (path === '/looktoday') setActiveMenu('LOOKTODAY');
-        else if (path === '/lookbook') setActiveMenu('LOOKBOOK');
-        else if (path === '/login' || path === '/sign-up' || path === '/sign-up-complete') setActiveMenu('LOGIN');
-        else if (path === '/lookbook') setActiveMenu('LOOKBOOK');
-        else setActiveMenu('');
-    }, [location.pathname]);
-
-    return (
-        <div className='menu'>
-            <div className='menu-gnb'>
-                <div
-                    onMouseEnter={() => setIsHovered(true)}
-                    onMouseLeave={() => setIsHovered(false)}
-                    onClick={() => navigate('/')}
-                >
-                    <img src={isHovered ? logoHover : logo} alt="logo" className={`logo-img ${isHovered ? 'hovered' : ''}`} />
-                </div>
-                <div className='menu-group'>
-                    <div
-                        onClick={() => { setActiveMenu('WEATHER'); navigate('/today-weather'); }}
-                        className={activeMenu === 'WEATHER' ? 'active-menu' : ''}
-                    >
-                        WEATHER
-                    </div>
-                    <div
-                        onClick={() => { setActiveMenu('LOOKTODAY'); navigate('/looktoday'); }}
-                        className={activeMenu === 'LOOKTODAY' ? 'active-menu' : ''}
-                    >
-                        LOOKTODAY
-                    </div>
-                    <div
-                        onClick={() => { setActiveMenu('LOOKBOOK'); navigate('/lookbook'); }}
-                        className={activeMenu === 'LOOKBOOK' ? 'active-menu' : ''}
-                    >
-                        LOOKBOOK
-                    </div>
-                    <div
-                        onClick={() => { setActiveMenu('LOGIN'); navigate('/login'); }}
-                        className={activeMenu === 'LOGIN' ? 'active-menu' : ''}
-                    >
-                        LOGIN
-                    </div>
-                </div>
             </div>
           )}
         </div>
